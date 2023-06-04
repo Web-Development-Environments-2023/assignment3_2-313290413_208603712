b0baed4c6fd54c6f6d95332dc59c7ef6ff2b2dfd
Erez Kimmel <105929916+ErezKimmel@users.noreply.github.com>
2023-06-04 23:24:26 +0300

    Update recipes.js

--- routes/recipes.js
+++ routes/recipes.js
@@ -10,11 +10,42 @@ router.get("/", (req, res) => res.send("im here"));
  */
 router.get("/:recipeId", async (req, res, next) => {
   try {
-    const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
+    const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId, 0);
     res.send(recipe);
-  } catch (error) {
+    
+  }
+  catch (error) {
     next(error);
   }
 });
+
+
+/**
+ * This path returns a list of recipes coming from the external API after using its search engine
+ */
+router.get("/search_query", async (req,res,next) => {
+  try{
+    const search= await recipes_utils.apiSearchQuery(req.body.query, req.body.number, req.body.cuisine, req.body.diet, req.body.intolerance);
+    res.status(200).send(search.data);
+  }
+  catch(error){
+    next(error); 
+  }
+});
+
+
+/**
+ * This path generates a list of 3 random recipes
+ */
+router.get("/random", async (req, res, next) => {
+  try {
+    let get_random = await recipes_utils.getRandomRecipes();
+    res.send(get_random);
+  } catch (error) {
+    next(error);
+  }
+})
+
+
 
 module.exports = router;
